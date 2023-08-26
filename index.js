5058fa86de99501efdb9aca0e7963424fe54256e
omkar-deshmukh1804 <58912751+omkar-deshmukh1804@users.noreply.github.com>
2023-08-26 18:40:52 +0000

    exercise-06

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 // ex-02
 var readlineSync = require('readline-sync');
+let score = 0
 // var userName = readlineSync.question("Please enter your name")
 // console.log(userName)
 
@@ -17,11 +18,25 @@ var readlineSync = require('readline-sync');
 // console.log(`Hi ${userName}. Welcome to NeoG Camp`)
 
 //ex-05
-var ageCheck = readlineSync.question("What's my age?")
-if (isNaN(ageCheck)){
-  console.log("Please enter a number")
-}else if (ageCheck!=25){
-  console.log("Wrong Answer")
-}else{
-  console.log("You're correct!")
-}
+// var ageCheck = readlineSync.question("What's my age?")
+// if (isNaN(ageCheck)){
+//   console.log("Please enter a number")
+// }else if (ageCheck!=25){
+//   console.log("Wrong Answer")
+// }else{
+//   console.log("You're correct!")
+// }
+
+//ex-06
+// var cityCheck = readlineSync.question("What is my home city?")
+// let score = 0
+// if(!isNaN(cityCheck)){
+//   console.log("Please enter city name in AlphaNumeric Format only.")
+// }else if (cityCheck.toLowerCase()=='mumbai'){
+//   score+=1
+//   console.log("Correct Answer!")
+//   console.log("Your Score is:" + score)
+  
+// }else {
+//   console.log("Wrong Answer")
+// }
